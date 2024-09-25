609413b72a6e5d5f21093a76f16428ff89782670
Maria Isabel Tovar Pastrana <isabeltovarp.18@gmail.com>
2024-09-24 21:48:22 -0500

    modal

--- js/comun/crearPuntos.js
+++ js/comun/crearPuntos.js
@@ -2,7 +2,6 @@
 export class ManejarPuntos {
     constructor(scene) {
         this.scene = scene;
-        
         // Tiempo
         this.tiempo = 25;
         this.tiempoText = this.scene.add.text(0, 0, 'TIEMPO DE VIDA: 25', { fontSize: '32px', fill: '#fff' });
@@ -10,12 +9,17 @@ export class ManejarPuntos {
 
         // Puntuación
         this.score = 0;
-        this.scoreText = this.scene.add.text(16, 100, 'Score: 0', { fontSize: '32px', fill: '#fff' });
+        this.scoreText = this.scene.add.text(this.scene.scale.width - 200, 0, 'Score: 0', { fontSize: '32px', fill: '#fff' });
         this.scoreText.setScrollFactor(0);
+        this.modalVisible = false; // Agregar esta propiedad
+
+        this.diamantes = 0;
+        this.diamantesText = this.scene.add.text(this.scene.scale.width - 300, 100, 'Diamantes: 0', { fontSize: '32px', fill: '#fff' });
+        this.diamantesText.setScrollFactor(0);
 
         // Temporizador
         this.scene.time.addEvent({
-            delay: 1000, 
+            delay: 1000,
             callback: this.decrementarTiempo,
             callbackScope: this,
             loop: true
@@ -24,7 +28,7 @@ export class ManejarPuntos {
 
     crearMinerales() {
         const mineralGroup = this.scene.physics.add.group();
-    
+
         // Genera minerales en las tres posiciones Y (0, 300, 400)
         [0, 300, 400].forEach(yPos => {
             mineralGroup.createMultiple({
@@ -33,22 +37,22 @@ export class ManejarPuntos {
                 setXY: { x: Phaser.Math.Between(0, 600), y: yPos, stepX: 150 }  // Posiciones X separadas por 120 píxeles
             });
         });
-    
+
         this.configurarMinerales(mineralGroup);
         return mineralGroup;
     }
     crearCorazones() {
         const mineralGroup = this.scene.physics.add.group();
-    
+
         // Genera minerales en las tres posiciones Y (0, 300, 400)
         [0, 300, 400].forEach(yPos => {
             mineralGroup.createMultiple({
-                key: 'mineral',
+                key: 'corazon',
                 repeat: 4, // Genera 5 minerales por cada posición Y (cambia si necesitas más o menos)
                 setXY: { x: Phaser.Math.Between(0, 600), y: yPos, stepX: 150 }  // Posiciones X separadas por 120 píxeles
             });
         });
-    
+
         this.configurarCorazones(mineralGroup);
         return mineralGroup;
     }
@@ -64,8 +68,8 @@ export class ManejarPuntos {
             child.setScale(0.08);  // Escala del mineral
         });
     }
-    
-  
+
+
     crearOxigeno() {
         const oxigenGroup = this.scene.physics.add.group({
             key: 'oxigeno',
@@ -82,26 +86,72 @@ export class ManejarPuntos {
             child.setScale(0.1);
         });
     }
-
+    confirmAction() {
+        console.log('uuu')
+        this.tiempo = 25;
+        this.tiempoText.setText('TIEMPO DE VIDA: ' + this.tiempo);
+    }
     decrementarTiempo() {
         if (this.tiempo > 0) {
             this.tiempo -= 1;
             this.tiempoText.setText('TIEMPO DE VIDA: ' + this.tiempo);
         }
-      
+    
         if (this.tiempo <= 0 && !this.scene.instanciaPersonaje.jugador.isDead) {
-            this.scene.instanciaPersonaje.jugador.isDead = true;
-            this.scene.instanciaPersonaje.jugador.anims.play('mario-dead');
-            this.scene.instanciaPersonaje.jugador.setCollideWorldBounds(false);
-            this.scene.sound.add('gameover', { volume: 0.2 }).play();
-        
-            setTimeout(() => {
-                this.scene.instanciaPersonaje.jugador.setVelocityY(-350);
-            }, 100);
+            this.mostrarModal();
         }
     }
     
-    recolectarOxigeno(mario, oxigeno) {
+    mostrarModal() {
+        if (!this.modalVisible) {
+            this.modalVisible = true;
+            var modalOxigeno = new bootstrap.Modal(document.getElementById('modal'));
+            modalOxigeno.show();
+    
+            this.iniciarTemporizador(modalOxigeno);
+        }
+    }
+    
+    iniciarTemporizador(modalOxigeno) {
+        var aceptar = document.getElementById('comprarOxigeno');
+        var temporizador = document.getElementById('temporizador');
+        var diamantesIndex = document.getElementById('diamantes');
+        let contador = 5;
+        diamantesIndex.textContent = this.diamantes;
+        temporizador.textContent = contador;
+    
+        aceptar.onclick = () => {
+            modalOxigeno.hide(); 
+            this.confirmAction(); 
+            this.modalVisible = false;
+            clearInterval(tiempoModal);
+        };
+    
+        const tiempoModal = setInterval(() => {
+            contador--;
+            temporizador.textContent = contador;
+    
+            if (contador <= 0) {
+                clearInterval(tiempoModal);
+                modalOxigeno.hide();
+                this.modalVisible = false;
+                this.deteriorarJugador();
+            }
+        }, 1000);
+    }
+    
+    deteriorarJugador() {
+        this.scene.instanciaPersonaje.jugador.isDead = true;
+        this.scene.instanciaPersonaje.jugador.anims.play('mario-dead');
+        this.scene.instanciaPersonaje.jugador.setCollideWorldBounds(false);
+        this.scene.sound.add('gameover', { volume: 0.2 }).play();
+        this.scene.add.text(400,this.scene.scale.height - 400, '¡MORISTEEEEE!', { fontSize: '60px', fill: '#fff' });
+        setTimeout(() => {
+            this.scene.instanciaPersonaje.jugador.setVelocityY(-350);
+        }, 100);
+    }
+
+    recolectarOxigeno(jugador, oxigeno) {
         oxigeno.disableBody(true, true);
         this.tiempo = 25;
         this.tiempoText.setText('TIEMPO DE VIDA: ' + this.tiempo);
@@ -111,14 +161,17 @@ export class ManejarPuntos {
         this.scene.physics.add.overlap(this.scene.instanciaPersonaje.jugador, oxigenGroup, this.recolectarOxigeno, null, this);
     }
 
-    recolectarMineral(mario, mineral) {
-        mineral.disableBody(true, true); 
-        this.score += 10; 
+    recolectarMineral(jugador, mineral) {
+        mineral.disableBody(true, true);
+        this.score += 10;
+        this.diamantes += 1;
         this.scoreText.setText('Score: ' + this.score);
+        this.diamantesText.setText('Diamantes: ' + this.diamantes);
+
 
         if (this.score >= 60) {
             this.scoreText.setText('Score: ' + this.score + ' - NIVEL COMPLETADO');
-            this.showModal(); 
+            this.showModal();
         }
     }
 
--- juegoPixel/plataformasPixel.js
+++ juegoPixel/plataformasPixel.js
@@ -1,57 +1,30 @@
 // platforms.js
 export function createPlatforms(scene) {
   const platformPositions = [
-    // { x: 0, y: scene.scale.height - 7, scale: 0.4 },
-    { x: 50, y: scene.scale.height - 16, scale: 0.1, asset: 'dona' },
-
-    // { x: 193, y: scene.scale.height - 7, scale: 0.4 },
-    { x: 250, y: scene.scale.height - 16, scale: 0.1, asset: 'dona' },
-    { x: 420, y: scene.scale.height - 116, scale: 0.1, asset: 'dona' },
-
-    // { x: 579, y: scene.scale.height - 7, scale: 0.4},
-    // { x: 620, y: scene.scale.height - 7, scale: 0.4 },
-    // { x: 775, y: scene.scale.height - 7, scale: 0.4 },
-    { x: 900, y: scene.scale.height - 45, scale: 0.09, asset: 'dona' },
-    { x: 1070, y: scene.scale.height - 85, scale: 0.09, asset: 'dona' },
-    // { x: 1096, y: scene.scale.height - 110, scale: 0.4 },
-    // { x: 1200, y: scene.scale.height - 90, scale: 0.05, asset: 'flotante-dos' },
-    // { x: 1050, y: scene.scale.height - 240, scale: 0.5, asset: 'dona' },
-    { x: 650, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    { x: 820, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    { x: 990, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    { x: 1116, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    { x: 1400, y: scene.scale.height - 280, scale: 0.09, asset: 'dona' },
-    { x: 1500, y: scene.scale.height - 350, scale: 0.09, asset: 'dona' },
-    { x: 1250, y: scene.scale.height - 420, scale: 0.09, asset: 'dona-2' },
-    { x: 820, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    { x: 990, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    { x: 1116, y: scene.scale.height - 185, scale: 0.09, asset: 'dona-2' },
-    // { x: 570, y: scene.scale.height - 570, scale: 0.05, asset: 'flotante' },
-
-    // { x: 450, y: scene.scale.height - 185, scale: 0.1, asset: 'dona-2' },
-    { x: 298, y: scene.scale.height - 185, scale: 0.05, asset: 'flotante-dos' },
-    { x: 155, y: scene.scale.height - 200, scale: 0.05, asset: 'flotante-dos' },
-    { x: 50, y: scene.scale.height - 260, scale: 0.05, asset: 'flotante-dos' },
-    { x: 1050, y: scene.scale.height - 400, scale: 0.1, asset: 'dona' },
-    { x: 400, y: scene.scale.height - 370, scale: 0.1, asset: 'dona' },
-    { x: 600, y: scene.scale.height - 400, scale: 0.1, asset: 'dona' },
-    { x: 850, y: scene.scale.height - 402, scale: 0.1, asset: 'dona' },
-    { x: 150, y: scene.scale.height - 402, scale: 0.1, asset: 'dona' },
-
-    { x: 1100, y: scene.scale.height - 400, scale: 0.05, asset: 'flotante-dos' },
-    { x: 1205, y: scene.scale.height - 470, scale: 0.05, asset: 'flotante-dos' },
-    { x: 995, y: scene.scale.height - 580, scale: 0.5, asset: 'flotante' },
-
-
-    // { x: 600, y: scene.scale.height - 270, scale: 0.4, asset: 'suelo-dos' },
-
-    // { x: 400, y: scene.scale.height - 200, scale: 0.4 },
-    // { x: 300, y: scene.scale.height - 280, scale: 0.4 },
-    // { x: 700, y: scene.scale.height - 202, scale: 0.4 },
+    { x: 50, y: scene.scale.height - 16, scale: 0.1 },
+    { x: 250, y: scene.scale.height - 16, scale: 0.1 },
+    { x: 420, y: scene.scale.height - 116, scale: 0.1 },
+    { x: 650, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 820, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 990, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 1116, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 1400, y: scene.scale.height - 280, scale: 0.09 },
+    { x: 1500, y: scene.scale.height - 350, scale: 0.09 },
+    { x: 1250, y: scene.scale.height - 420, scale: 0.09, asset: 'suelo-flo' },
+    { x: 820, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 990, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 1116, y: scene.scale.height - 185, scale: 0.09, asset: 'suelo-flo' },
+    { x: 155, y: scene.scale.height - 200, scale: 0.05, asset: 'flotante' },
+    { x: 50, y: scene.scale.height - 260, scale: 0.05, asset: 'flotante' },
+    { x: 1050, y: scene.scale.height - 400, scale: 0.1 },
+    { x: 400, y: scene.scale.height - 370, scale: 0.1 },
+    { x: 600, y: scene.scale.height - 400, scale: 0.1 },
+    { x: 850, y: scene.scale.height - 402, scale: 0.1 },
+    { x: 150, y: scene.scale.height - 402, scale: 0.1 },
   ];
 
   platformPositions.forEach(pos => {
-    const asset = pos.asset || 'suelo';
+    const asset = pos.asset || 'suelo-flotante';
     scene.floor.create(pos.x, pos.y, asset).setOrigin(0, 1).setScale(pos.scale).refreshBody();
   });
 }
